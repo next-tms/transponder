5ed635fc0c72fe54b4c0613839f64180315a1246
Brody <brodyhoskins@users.noreply.github.com>
2023-08-27 13:16:05 -0700

    Validate `request.params`

--- spec/actions/graphql/query_spec.rb
+++ spec/actions/graphql/query_spec.rb
@@ -2,9 +2,22 @@
 
 # rubocop:disable RSpec/FilePath
 RSpec.describe(Transponder::Actions::GraphQL::Query) do
-  let(:params) { Hash[] }
+  let(:operation_name) { nil }
+  let(:query) { Faker::Alphanumeric }
+  let(:variables) { nil }
+  let(:params) do
+    {}.tap do |builder|
+      builder[:operation_name] = operation_name if operation_name
+      builder[:query] = query if query
+      builder[:variables] = variables if variables
+    end
+  end
   let(:response) { subject.call(params) }
 
-  it { expect(response).to(be_successful) }
+  context 'when query is blank' do
+    let(:query) { ['', nil].sample }
+
+    it { expect(response.status).to(eq(400)) }
+  end
 end
 # rubocop:enable RSpec/FilePath
