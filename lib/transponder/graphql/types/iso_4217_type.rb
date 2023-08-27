a57b4bedfab7d8ca64abcd794c6a68bbd51883f8
Brody <brodyhoskins@users.noreply.github.com>
2023-08-27 14:20:37 -0700

    Add `description:`s for ISO types

--- lib/transponder/graphql/types/iso_4217_type.rb
+++ lib/transponder/graphql/types/iso_4217_type.rb
@@ -6,7 +6,9 @@ module Transponder
   module GraphQL
     module Types
       class ISO4217Type < BaseEnum
-        ::Money::Currency.all.map(&:iso_code).uniq.each { |iso4217| value iso4217 }
+        ::Money::Currency.all.uniq(&:iso_code).sort_by(&:iso_code).each do |currency|
+          value currency.iso_code, description: currency.name
+        end
       end
     end
   end
