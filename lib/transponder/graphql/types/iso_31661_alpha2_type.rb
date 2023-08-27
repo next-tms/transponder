08938732a9e2f3ab6524b8d1063673d351db1c5f
Brody <brodyhoskins@users.noreply.github.com>
2023-08-27 14:40:01 -0700

    Link to ISO type definitions in `description:`

--- lib/transponder/graphql/types/iso_31661_alpha2_type.rb
+++ lib/transponder/graphql/types/iso_31661_alpha2_type.rb
@@ -4,6 +4,8 @@ module Transponder
   module GraphQL
     module Types
       class ISO31661Alpha2Type < BaseEnum
+        description "See https://www.rubydoc.info/gems/active_utils/#{ActiveUtils::VERSION}/ActiveUtils/Country"
+
         ActiveUtils::Country::COUNTRIES.map { |country| country[:alpha2] }.uniq.sort.map do |alpha2|
           description = ActiveUtils::Country::COUNTRIES
                         .select { |country| country[:alpha2] == alpha2 }
--- lib/transponder/graphql/types/iso_31661_alpha3_type.rb
+++ lib/transponder/graphql/types/iso_31661_alpha3_type.rb
@@ -4,6 +4,8 @@ module Transponder
   module GraphQL
     module Types
       class ISO31661Alpha3Type < BaseEnum
+        description "See https://www.rubydoc.info/gems/active_utils/#{ActiveUtils::VERSION}/ActiveUtils/Country"
+
         ActiveUtils::Country::COUNTRIES.map { |country| country[:alpha3] }.uniq.sort.map do |alpha3|
           description = ActiveUtils::Country::COUNTRIES
                         .select { |country| country[:alpha3] == alpha3 }
