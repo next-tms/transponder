697974dcd2c1f50f0f1ec738b92bdf3f6c8c86e7
Brody <brodyhoskins@users.noreply.github.com>
2023-10-29 12:33:35 -0700

    Use `URI.parse`

--- lib/transponder/graphql/types/carrier_credential_input_type.rb
+++ lib/transponder/graphql/types/carrier_credential_input_type.rb
@@ -17,7 +17,7 @@ module Transponder
           if type == :selenoid
             return FreightKit::Credential.new(
               type: :selenoid,
-              base_url: ENV.fetch('SELENOID_BASE_URL'),
+              base_url: URI.parse(ENV.fetch('SELENOID_BASE_URL')),
               browser: :chrome,
             )
           end
