1f283a1d838bbfec8e3f70a8e9e272c1f7d655f1
Brody Hoskins <brodyhoskins@users.noreply.github.com>
2024-02-07 14:41:09 -0800

    Add `fetchTracking` error handling (#54)

--- lib/transponder/graphql/queries/fetch_tracking.rb
+++ lib/transponder/graphql/queries/fetch_tracking.rb
@@ -4,6 +4,16 @@ module Transponder
   module GraphQL
     module Queries
       class FetchTracking < ::GraphQL::Schema::Resolver
+        EXCEPTIONS = {
+                       ::FreightKit::InvalidCredentialsError => { code: 'INVALID_CREDENTIALS_ERROR' },
+                       ::FreightKit::ResponseError => { code: 'API_ERROR', message: 'API error' },
+                       ::NotImplementedError => {
+                                                  code: 'NOT_IMPLEMENTED_ERROR',
+                                                  message: "Carrier doesn't support fetching tracking via API"
+                                                },
+                       Types::CarrierInputType::CarrierNotFoundError => { code: 'CARRIER_NOT_KNOWN_ERROR' }
+                     }
+
         type ::Transponder::GraphQL::Types::TrackingResponseType, null: true
 
         argument :carrier, Types::CarrierInputType, required: true
@@ -11,9 +21,21 @@ module Transponder
 
         def resolve(carrier:, tracking_number:)
           carrier.find_tracking_info(tracking_number)
-        rescue NotImplementedError
-          message = 'Tracking not supported by carrier'
-          raise ::GraphQL::ExecutionError, message, extensions: { code: 'NOT_IMPLEMENTED_ERROR' }
+        rescue *EXCEPTIONS.keys => e
+          raise_error(e)
+        end
+
+        private
+
+        def raise_error(error)
+          code = EXCEPTIONS[error.class][:code]
+          message = if EXCEPTIONS[error.class][:message].present?
+                      EXCEPTIONS[error.class][:message]
+                    elsif error.message != error.class.to_s
+                      error.message
+                    end
+
+          context.add_error(::GraphQL::ExecutionError.new(message, extensions: { code: code }))
         end
       end
     end
