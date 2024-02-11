3a009e80fdaac8245f309ad959f7f1990e91ac6c
Brody Hoskins <brodyhoskins@users.noreply.github.com>
2024-02-10 16:50:18 -0800

    Always return `GraphQL::ExecutionError` (#57)

--- lib/transponder/graphql/mutations/create_pickup.rb
+++ lib/transponder/graphql/mutations/create_pickup.rb
@@ -4,6 +4,16 @@ module Transponder
   module GraphQL
     module Mutations
       class CreatePickup < ::GraphQL::Schema::Resolver
+        EXCEPTIONS = {
+                       ::FreightKit::ExpiredCredentialsError => { code: 'INVALID_CREDENTIALS_ERROR' },
+                       ::FreightKit::InvalidCredentialsError => { code: 'INVALID_CREDENTIALS_ERROR' },
+                       ::FreightKit::ResponseError => { code: 'API_ERROR' },
+                       ::FreightKit::UnserviceableAccessorialsError => { code: 'NOT_SERVICEABLE_ERROR' },
+                       ::FreightKit::UnserviceableError => { code: 'NOT_SERVICEABLE_ERROR' },
+                       ::NotImplementedError => { code: 'NOT_IMPLEMENTED_ERROR' },
+                       Types::CarrierInputType::CarrierNotFoundError => { code: 'CARRIER_NOT_KNOWN_ERROR' }
+                     }
+
         type ::Transponder::GraphQL::Types::PickupResponseType, null: false
 
         argument :carrier, Types::CarrierInputType, required: true
@@ -16,18 +26,42 @@ module Transponder
         argument :service, String, required: false
         argument :shipment, ::Transponder::GraphQL::Types::ShipmentInputType, required: true
 
-        def resolve(carrier:, delivery_from: nil, delivery_to: nil, dispatcher:, pickup_from:, pickup_to:, scac:,
-          service: nil, shipment:)
-          carrier.create_pickup(
-            delivery_from:,
-            delivery_to:,
-            dispatcher:,
-            pickup_from:,
-            pickup_to:,
-            scac:,
-            service:,
-            shipment:,
-          )
+        def resolve(
+          carrier:,
+          delivery_from: nil,
+          delivery_to: nil,
+          dispatcher:,
+          pickup_from:,
+          pickup_to:,
+          scac:,
+          service: nil,
+          shipment:
+        )
+          result = carrier
+                   .create_pickup(
+                     delivery_from:,
+                     delivery_to:,
+                     dispatcher:,
+                     pickup_from:,
+                     pickup_to:,
+                     scac:,
+                     service:,
+                     shipment:,
+                   )
+          return result if result.error.blank?
+
+          raise result.error
+        rescue *EXCEPTIONS.keys => e
+          raise_error(e)
+        end
+
+        private
+
+        def raise_error(error)
+          code = EXCEPTIONS[error.class][:code]
+          message = error.message if error.message != error.class.to_s
+
+          context.add_error(::GraphQL::ExecutionError.new(message, extensions: { code: }))
         end
       end
     end
--- lib/transponder/graphql/types/query_type.rb
+++ lib/transponder/graphql/types/query_type.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require 'freight_kit'
-require 'freight_kit-next'
-
 module Transponder
   module GraphQL
     module Types
